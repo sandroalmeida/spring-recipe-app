6210520b18d09487b244b7818289ce29ebaa778f
sandroalmeida <sandrodotbuild@gmail.com>
2019-01-22 15:18:38 -0800

    Added UnitOfMeasure Class.

---
+++ src/main/java/info/sandroalmeida/springrecipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,35 @@
+package info.sandroalmeida.springrecipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by sandro on 22/01/19
+ */
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
