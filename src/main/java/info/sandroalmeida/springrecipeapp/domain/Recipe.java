bdb262e290906d18857f69c97f88316021789212
sandroalmeida <sandrodotbuild@gmail.com>
2019-01-22 15:04:18 -0800

    Added Ingredient Class.

--- src/main/java/info/sandroalmeida/springrecipeapp/domain/Recipe.java
+++ src/main/java/info/sandroalmeida/springrecipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package info.sandroalmeida.springrecipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by sandro on 22/01/19
@@ -21,6 +22,9 @@ public class Recipe {
     private String directions;
     // todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -90,6 +94,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
