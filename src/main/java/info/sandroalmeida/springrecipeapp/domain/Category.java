ec48d991e96704ebfd3e365197da32760268e8b3
sandroalmeida <sandrodotbuild@gmail.com>
2019-01-23 10:07:42 -0800

    Added Category Class.

---
+++ src/main/java/info/sandroalmeida/springrecipeapp/domain/Category.java
@@ -0,0 +1,44 @@
+package info.sandroalmeida.springrecipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by sandro on 23/01/19
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
