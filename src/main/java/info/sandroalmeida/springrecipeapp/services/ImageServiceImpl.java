d8031fc194e1fc17c31d4ece6ace828923d153d1
sandroalmeida <sandrodotbuild@gmail.com>
2019-05-02 08:14:57 -0700

    Added save image on database process

--- src/main/java/info/sandroalmeida/springrecipeapp/services/ImageServiceImpl.java
+++ src/main/java/info/sandroalmeida/springrecipeapp/services/ImageServiceImpl.java
@@ -1,8 +1,13 @@
 package info.sandroalmeida.springrecipeapp.services;
 
+import info.sandroalmeida.springrecipeapp.domain.Recipe;
+import info.sandroalmeida.springrecipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by sandro on 02/05/19
@@ -11,8 +16,36 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
